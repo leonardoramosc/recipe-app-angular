270c681a02c0f7f2e30027103a52972b1b915287
leonardoramosc <leorrc96@gmail.com>
2021-01-28 09:29:54 -0400

    Recipe model was created and with that now we are able to insert several recipes into the recipe-list component, so also some styles were created inside the recipe-list to display these recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    {
+      name: 'Test Recipe',
+      description: 'This is a test',
+      image: 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg'
+    },
+    {
+      name: 'Pizza',
+      description: 'Delicious pizza',
+      image: 'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-5.jpg'
+    }
+  ];
 
   constructor() { }
 
