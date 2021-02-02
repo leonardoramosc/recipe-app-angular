899eba34ca4e629a5c3484e7b3d99f116ce9e424
leonardoramosc <leorrc96@gmail.com>
2021-02-02 12:47:24 -0400

    Now in the recipe-edit component we are able to know if the user wants to add a new recipe or edit an existing

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.scss']
 })
 export class RecipeEditComponent implements OnInit {
+  editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // Check if the user want to edit a recipe
+    this.route.params.subscribe((params: Params) => {
+      // if there is not any id param, it means that the user wants to add a new, otherwise wants to edit one.
+      this.editMode = params.id !== undefined;
+    });
   }
 
 }
